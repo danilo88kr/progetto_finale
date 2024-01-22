07afc51f0adcfc2ed19d07c7bbd1fd971ac2a8c6
danilo88kr <danilo88kr@gmail.com>
2024-01-22 12:49:22 +0100

    progetto finale completato

---
+++ resources/views/components/masthead.blade.php
@@ -0,0 +1,14 @@
+<div class="container fluid my-5 h-100">
+    <div class=" row d-flex justify-content-center p-5 text-center rounded rounded-2  header-custom">
+      
+       <h1 class="text-body-dark text-black  mb-5 fw-bold display-4 mt-8">{{$title}}</h1>
+      <p class="col-lg-6 mx-auto mb-4 fw-bold my-4">
+        
+      </p>
+      <button class="btn-custom " type="button">
+        <a class="nav-link" href="{{route('announcements.create')}}">{{__('ui.create')}}</a>
+      </button>
+    </div>
+  </div>
+ 
+
